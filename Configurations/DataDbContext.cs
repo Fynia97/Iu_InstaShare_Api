28e0c87f2577538edcac33ee4be91bf407235458
Fynia97 <annabel@gaul05.de>
2024-04-09 11:11:47 +0200

    [Add] DataDbContext, first migration

---
+++ Configurations/DataDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Iu_InstaShare_Api.Configurations
+{
+    //For Code-first, migrate models to db, use: dotnet ef migrations add NAME_OF_MIGRATION, check migration, then use dotnet ef database update
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext() { }
+
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=InstaShare;Integrated Security=True");
+            }
+        }
+
+        //For every model: public DbSet<ADDTYPE> ADDNAME { get; set; }
+
+    }
+}
