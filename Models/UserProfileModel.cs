ee9eb4a6637bee319998bc94107927e0cc888670
Fynia97 <annabel@gaul05.de>
2024-04-23 08:26:40 +0200

    [Add] UserProfileModel

---
+++ Models/UserProfileModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Iu_InstaShare_Api.Models
+{
+    public class UserProfileModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Password { get; set; } = "";
+
+        public bool Active { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Today;
+        public DateTime? UpdatedAt { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+    }
+}
