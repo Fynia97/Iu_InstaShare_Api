4d35caaa5a67fc10912fa28c900eafac44e79e54
Fynia97 <annabel@gaul05.de>
2024-04-25 22:13:24 +0200

    [Migrate]

---
+++ Migrations/20240425201050_AddAdressToUser.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Iu_InstaShare_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdressToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "UserProfiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Zip",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Zip",
+                table: "UserProfiles");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "UserProfiles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
